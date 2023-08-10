9a3bfd90e4a289ce1fd45323f8674665835ee578
du-ulises <du.ulises@gmail.com>
2023-08-10 16:56:44 -0600

    Análisis de rutas y componentes en React

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,23 @@
+import {Home} from '../Home/index.jsx'
+import {MyAccount} from "../MyAccount/index.jsx";
+import {MyOrder} from "../MyOrder/index.jsx";
+import {MyOrders} from "../MyOrders/index.jsx";
+import {NotFound} from "../NotFound/index.jsx";
+import {SignIn} from "../SignIn/index.jsx";
+import '../../App.css'
+
+function App() {
+
+  return (
+      <div className="bg-red-100">
+          <Home/>
+          <MyAccount/>
+          <MyOrder/>
+          <MyOrders/>
+          <NotFound/>
+          <SignIn/>
+      </div>
+  )
+}
+
+export default App
